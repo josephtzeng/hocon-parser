e32f06a58c5d68504bfd5b4d414bff2e7657df90
Josh Soref <jsoref@users.noreply.github.com>
2021-06-06 19:35:04 -0400

    spelling: defaults
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- tests/lib/utils.options.js
+++ tests/lib/utils.options.js
@@ -1,13 +1,13 @@
 test.func (_.options)
-    .should ("return options defined by defualts")
+    .should ("return options defined by defaults")
     .given ({ a: 1 }, { a: 3, b: 4 })
     .returns ({ a: 3 })
 
-    .should ("return use an empty object if defualts is null")
+    .should ("return use an empty object if defaults is null")
     .given (null, { a: 3, b: 4 })
     .returns ({})
 
-    .should ("throw if defualts is a primitive")
+    .should ("throw if defaults is a primitive")
     .given (3, { a: 3, b: 4 })
     .throws ()
 ;
